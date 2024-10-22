710a45ff19e0474986bfbec6611131669089c03c
Julian Jaimes <julian.jaimesr@gmail.com>
2024-10-21 23:36:45 -0500

    fix marca delete

--- routes/api.php
+++ routes/api.php
@@ -68,7 +68,7 @@ Route::group(['middleware' => 'api'], function($router){
     Route::resource('/baterias-equipo', BateriaEquipoController::class);
     Route::resource('/cliente', ClienteController::class);
     Route::resource('/contrato', ContratoController::class);
-    Route::delete('/marcas/delete-multiple', [MarcaController::class, 'destroyMultiple']);
+    Route::delete('/marca/delete-multiple', [MarcaController::class, 'destroyMultiple']);
 
 
 });
